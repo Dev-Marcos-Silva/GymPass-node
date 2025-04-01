522f460fff38fdda65fa9e85843b625e3fd353bb
Dev-Marcos-Silva <dev.marcosvieira@gmail.com>
2025-04-01 01:20:01 -0300

    created use-cases

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,10 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
 import { z } from "zod";
+import { registerUseCase } from "@/use-cases/register";
 
-export async function registers( request:FastifyRequest, reply:FastifyReply ){
+export async function registers( request: FastifyRequest, reply: FastifyReply ){
 
     const registerBodySchema = z.object({
         name: z.string(),
@@ -12,14 +14,17 @@ export async function registers( request:FastifyRequest, reply:FastifyReply ){
 
     const { name, email, password } = registerBodySchema.parse(request.body)
 
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: password
-        }
-    })
+    try{
+        
+        registerUseCase({name, email, password})
 
+    }catch(err){
+
+        return reply.status(409).send()
+
+    }
+
+    
     return reply.status(201).send()
 
 }
