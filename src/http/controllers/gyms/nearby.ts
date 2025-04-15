677270497d68d76d744125efa76d7a7bf040b8eb
Dev-Marcos-Silva <dev.marcosvieira@gmail.com>
2025-04-15 00:48:16 -0300

    created controllers gyms

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,25 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+import { makeFetchNearbyGymsUseCase } from "@/use-cases/factories/make-fetch-nearby-gyms-use-case";
+
+export async function nearby( request: FastifyRequest, reply: FastifyReply ){
+
+            
+    const nearbyQuerySchema = z.object({
+
+        userLatitude: z.number().refine((value)=>{
+            return Math.abs(value) <= 90
+        }),
+        userLongitude: z.number().refine((value)=>{
+            return Math.abs(value) <= 180
+        }),
+    })
+
+    const { userLatitude, userLongitude } = nearbyQuerySchema.parse(request.body)
+    
+    const fetchGymsUseCase = makeFetchNearbyGymsUseCase()
+
+    const { gyms } =await fetchGymsUseCase.execute({ userLatitude, userLongitude })
+
+    return reply.status(200).send({gyms,})
+}
