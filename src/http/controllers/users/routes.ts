20449ec033a673840ce13c755220bef441fcad66
Dev-Marcos-Silva <dev.marcosvieira@gmail.com>
2025-04-16 00:23:32 -0300

    add RBAC in project

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { authenticate } from "./authenticate";
 import { profile } from "./profile";
 import { registers } from "./register";
 import { verifyJWT } from "../../middlewares/verify-jwt";
+import { refresh } from "./refresh";
 
 export async function usersRoutes( app: FastifyInstance ){
 
     app.post('/users', registers)
     app.post('/sessions', authenticate)
+
+    app.patch('/token/refresh', refresh)
 
     /**Authentication**/
 
